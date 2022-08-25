1f0b06ed785808787debd3b55ee4a5d0b5c0ed9a
jason <howdoicometothis@gmail.com>
2022-08-25 20:45:25 +0800

    add mapping for hop and add surround plugin

---
+++ lua/configs/mapping.lua
@@ -0,0 +1,3 @@
+--[[ for hop ]]--
+vim.keymap.set('', '<leader>hc', "<cmd>lua require'hop'.hint_char2()<cr>", {})
+vim.keymap.set('', '<leader>hw', "<cmd>lua require'hop'.hint_words()<cr>", {})
--- lua/configs/pack.lua
+++ lua/configs/pack.lua
@@ -18,5 +18,16 @@ return require('packer').startup(function(use)
 
   use {'kdheepak/lazygit.nvim'}
   use 'neovim/nvim-lspconfig'
+
+
+	use{
+			"kylechui/nvim-surround",
+			tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+			config = function()
+					require("nvim-surround").setup({
+							-- Configuration here, or leave empty to use defaults
+					})
+			end
+	}
 end)
 
