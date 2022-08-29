56bc807427f7e303c1274fd6c0d7951a48594bee
jason <howdoicometothis@gmail.com>
2022-08-29 16:28:28 +0800

    change indent

--- lua/configs/pack.lua
+++ lua/configs/pack.lua
@@ -21,13 +21,13 @@ return require('packer').startup(function(use)
 
 
 	use{
-			"kylechui/nvim-surround",
-			tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-			config = function()
-					require("nvim-surround").setup({
-							-- Configuration here, or leave empty to use defaults
-					})
-			end
+    "kylechui/nvim-surround",
+    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+      require("nvim-surround").setup({
+      -- Configuration here, or leave empty to use defaults
+      })
+    end
 	}
 
   use "lukas-reineke/indent-blankline.nvim"
