81f1fd8ec1da3da282ebd9386ebbf3d1ca5ebccc
jason <howdoicometothis@gmail.com>
2022-09-08 13:17:57 +0000

    remap z to redraw the screen in neotree

--- lua/configs/plugin_configs.lua
+++ lua/configs/plugin_configs.lua
@@ -66,3 +66,15 @@ local Terminal  = require('toggleterm.terminal').Terminal
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction='float' })
 
 vim.keymap.set("n", "<leader>lg", function() lazygit:toggle() end, {noremap = true, silent = true})
+
+
+
+--[[ neotree ]]--
+
+require('neo-tree').setup({
+  window = {
+    mappings = {
+      ["z"] = false
+    }
+  }
+})
