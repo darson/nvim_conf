c7eb71f0c02a3060b5651484f2bb340683496d28
jason <howdoicometothis@gmail.com>
2022-09-10 23:36:43 +0800

    use desert colorscheme

--- init.lua
+++ init.lua
@@ -8,6 +8,7 @@ vim.o.fileencodings = "utf-8,ucs-bom,gb18030,cp936"
 vim.o.fileformats = "unix,dos,mac"
 vim.o.wildmode = "longest:full,full"
 vim.o.completeopt = "menu"
+vim.cmd[[colorscheme desert]]
 
 -- no <tab> insert <space>
 vim.o.expandtab = true
