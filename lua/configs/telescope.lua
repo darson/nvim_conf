3f7f5cd8490e1bde086264ae0386368a5ff2f780
jason <howdoicometothis@gmail.com>
2022-09-20 13:12:04 +0000

    add multiselection to buffers.delete in telescope

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -31,14 +31,26 @@ local function my_buffer()
       local delete_buf = function()
         local action_state = require('telescope.actions.state')
         local actions = require('telescope.actions')
-        local selection = action_state.get_selected_entry()
-        actions.close(prompt_bufnr)
-        vim.api.nvim_buf_delete(selection.bufnr, { force = true })
+
+        local current_picker = action_state.get_current_picker(prompt_bufnr)
+        local multi_selection = current_picker:get_multi_selection()
+
+        if next(multi_selection) == nil then
+          local selection = action_state.get_selected_entry()
+          actions.close(prompt_bufnr)
+          vim.api.nvim_buf_delete(selection.bufnr, { force = true })
+        else
+          actions.close(prompt_bufnr)
+          for _, selection in ipairs(multi_selection) do
+            vim.api.nvim_buf_delete(selection.bufnr, { force = true })
+          end
+        end
+
         my_buffer()
       end
 
-      map('i', '<c-d>', delete_buf)
-      map('n', 'd', delete_buf)
+      map('i', '<c-x>', delete_buf)
+      map('n', 'x', delete_buf)
 
       return true
     end
